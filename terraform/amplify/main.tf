7930fa0b322ea676bce021c2f57d7e2d682085d3
kesompochy <kesompochy@gmail.com>
2023-05-20 23:42:48 +0900

    init

---
+++ terraform/amplify/main.tf
@@ -0,0 +1,49 @@
+
+
+resource "aws_amplify_app" "isucon_portal" {
+  name       = "isucon-portal"
+  repository = "https://github.com/kesompochy/cto-a-isucon-portal"
+  access_token = file("${path.module}/pat.txt")
+
+  build_spec = <<-EOT
+    version: 0.1
+    frontend:
+      phases:
+        preBuild:
+          commands:
+            - cd frontend
+            - yarn
+        build:
+          commands:
+            - yarn run build
+      artifacts:
+        baseDirectory: dist
+        files:
+          - '**/*'
+      cache:
+        paths:
+          - node_modules/**/*
+  EOT
+
+  enable_auto_branch_creation = true
+
+  auto_branch_creation_patterns = [
+    "portal/**",
+  ]
+
+  # The default rewrites and redirects added by the Amplify Console.
+  custom_rule {
+    source = "/<*>"
+    status = "404"
+    target = "/index.html"
+  }
+
+  environment_variables = {
+    ENV = "test"
+  }
+}
+
+resource "aws_amplify_branch" "main" {
+  app_id  = aws_amplify_app.isucon_portal.id
+  branch_name = "main"
+}
