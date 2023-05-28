1dc29498765c92928efdba7a0663aa6fb754c4df
kesompochy <kesompochy@gmail.com>
2023-05-28 16:09:05 +0900

    過去のスコアも残すのでtimestampをrange_keyとして登録

--- terraform/api.tf
+++ terraform/api.tf
@@ -2,10 +2,16 @@ resource "aws_dynamodb_table" "portal_scores" {
   name         = "portal_scores"
   billing_mode = "PAY_PER_REQUEST"
   hash_key     = "team_id"
+  range_key    = "timestamp"
 
   attribute {
     name = "team_id"
     type = "S"
+  }
+
+  attribute {
+    name = "timestamp"
+    type = "N"
   }
 }
 
