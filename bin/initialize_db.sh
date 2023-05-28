89e9993339bd0abcd240ed15e3a0854ecb48a599
kesompochy <kesompochy@gmail.com>
2023-05-28 16:08:07 +0900

    テーブルごと消したほうが初期化が手っ取り早い

--- bin/initialize_db.sh
+++ bin/initialize_db.sh
@@ -1,5 +1,23 @@
 #!/bin/bash
 
+# Table name
+TABLE_NAME="portal_scores"
+
+# Delete the table
+aws dynamodb delete-table --table-name $TABLE_NAME --region ap-northeast-1
+
+# Wait until the table is deleted
+aws dynamodb wait table-not-exists --table-name $TABLE_NAME --region ap-northeast-1
+
+# Create the table
+aws dynamodb create-table --table-name $TABLE_NAME \
+  --attribute-definitions AttributeName=team_id,AttributeType=S AttributeName=timestamp,AttributeType=N \
+  --key-schema AttributeName=team_id,KeyType=HASH AttributeName=timestamp,KeyType=RANGE \
+  --billing-mode PAY_PER_REQUEST --region ap-northeast-1
+
+# Wait until the table is created
+aws dynamodb wait table-exists --table-name $TABLE_NAME --region ap-northeast-1
+
 # Check if TEAM_COUNT is set
 if [ -z "${TEAM_COUNT}" ]; then
   echo "Please set the TEAM_COUNT environment variable."
