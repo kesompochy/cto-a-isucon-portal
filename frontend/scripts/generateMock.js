26f1b1bbc0337c162140dcd9a363ff5f7cabfb05
kesompochy <kesompochy@gmail.com>
2023-06-07 00:02:01 +0900

    パラメータ指定せずともモックデータをいい感じに生成してほしい

--- frontend/scripts/generateMock.js
+++ frontend/scripts/generateMock.js
@@ -2,11 +2,12 @@ import fs from 'fs';
 
 // ミリ秒を秒に変換するための定数
 const MS_TO_SEC = 1e3;
-// 時間を調整するための定数（3時間を秒単位で表現）
-const THREE_HOURS_IN_SEC = 3 * 60 * 60;
 
 // モックデータを生成するための関数
-const generateMockData = (teamCount) => {
+const generateMockData = (sinceHour, teamCount) => {
+	// 時間を調整するための定数（3時間を秒単位で表現）
+	const THREE_HOURS_IN_SEC = sinceHour * 60 * 60;
+
 	const currentTimeSec = Math.floor(Date.now() / MS_TO_SEC);
 	const threeHoursAgoTimestamp = currentTimeSec - THREE_HOURS_IN_SEC;
 
@@ -32,10 +33,11 @@ const generateMockData = (teamCount) => {
 };
 
 // スクリプトの引数から出力ファイルパスとチーム数を取得する
-const [outputFilePath, teamCount] = process.argv.slice(2);
+const [hours = 8, teamCount = 10, outputFilePath = './src/mocks/scores.json'] =
+	process.argv.slice(2);
 
 // モックデータを生成する
-const mockData = generateMockData(Number(teamCount));
+const mockData = generateMockData(Number(hours), Number(teamCount));
 
 // JSONファイルとして保存する
 fs.writeFileSync(outputFilePath, JSON.stringify(mockData, null, 4));
