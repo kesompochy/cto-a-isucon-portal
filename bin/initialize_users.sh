3c9e06e3b171df0b1530ec6bd4537a8c73e32769
kesompochy <kesompochy@gmail.com>
2023-06-11 04:44:38 +0900

    signupとconfirmもスクリプトで行っちゃう

--- bin/initialize_users.sh
+++ bin/initialize_users.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 
 USER_POOL_ID="ap-northeast-1_owwW23cX9"
+CLIENT_ID="28pbs1v5v549utukg74m89o2q3"
 REGION="ap-northeast-1"
 NUM_TEAMS=$1
 
@@ -14,25 +15,33 @@ done
 # admin ユーザーの作成
 ADMIN_USERNAME="admin"
 ADMIN_PASSWORD=$(openssl rand -base64 12 | sed 's/[+/=]/#/g')'1!'
-aws cognito-idp admin-create-user \
+aws cognito-idp sign-up \
+  --client-id ${CLIENT_ID} \
+  --username ${ADMIN_USERNAME} \
+  --password ${ADMIN_PASSWORD} \
+  --region ${REGION} > /dev/null 2>&1
+
+aws cognito-idp admin-confirm-sign-up \
   --user-pool-id ${USER_POOL_ID} \
   --username ${ADMIN_USERNAME} \
-  --temporary-password ${ADMIN_PASSWORD} \
-  --user-attributes Name="email",Value="admin@example.com" \
-  --region ${REGION} >/dev/null 2>&1
+  --region ${REGION} > /dev/null 2>&1
 
 echo "Admin: Username=${ADMIN_USERNAME}, Password=${ADMIN_PASSWORD}"
 
 # チームごとのユーザーの作成
-for ((i=0; i<=NUM_TEAMS-1; i++)); do
+for ((i=1; i<=NUM_TEAMS; i++)); do
   TEAM_USERNAME="team${i}"
   TEAM_PASSWORD=$(openssl rand -base64 12 | sed 's/[+/=]/#/g')'1!'
-  aws cognito-idp admin-create-user \
+  aws cognito-idp sign-up \
+    --client-id ${CLIENT_ID} \
+    --username ${TEAM_USERNAME} \
+    --password ${TEAM_PASSWORD} \
+    --region ${REGION} > /dev/null 2>&1
+
+  aws cognito-idp admin-confirm-sign-up \
     --user-pool-id ${USER_POOL_ID} \
     --username ${TEAM_USERNAME} \
-    --temporary-password ${TEAM_PASSWORD} \
-    --user-attributes Name="email",Value="team${i}@example.com" \
-    --region ${REGION} >/dev/null 2>&1
+    --region ${REGION} > /dev/null 2>&1
 
   echo "Team ${i}: Username=${TEAM_USERNAME}, Password=${TEAM_PASSWORD}"
 done
