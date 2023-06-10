4bdfb479dd0b369297e23a072174b08fbbbdb9c6
kesompochy <kesompochy@gmail.com>
2023-06-11 03:09:42 +0900

    ユーザーの初期化スクリプトを作成

---
+++ bin/initialize_users.sh
@@ -0,0 +1,38 @@
+#!/bin/bash
+
+USER_POOL_ID="ap-northeast-1_owwW23cX9"
+REGION="ap-northeast-1"
+NUM_TEAMS=$1
+
+# 既存のユーザーを削除する
+EXISTING_USERS=$(aws cognito-idp list-users --user-pool-id ${USER_POOL_ID} --region ${REGION} --query "Users[].Username" --output text)
+for EXISTING_USER in ${EXISTING_USERS}; do
+    aws cognito-idp admin-delete-user --user-pool-id ${USER_POOL_ID} --username ${EXISTING_USER} --region ${REGION}
+    echo "Deleted user: ${EXISTING_USER}"
+done
+
+# admin ユーザーの作成
+ADMIN_USERNAME="admin"
+ADMIN_PASSWORD=$(openssl rand -base64 12 | sed 's/[+/=]/#/g')'1!'
+aws cognito-idp admin-create-user \
+  --user-pool-id ${USER_POOL_ID} \
+  --username ${ADMIN_USERNAME} \
+  --temporary-password ${ADMIN_PASSWORD} \
+  --user-attributes Name="email",Value="admin@example.com" \
+  --region ${REGION} >/dev/null 2>&1
+
+echo "Admin: Username=${ADMIN_USERNAME}, Password=${ADMIN_PASSWORD}"
+
+# チームごとのユーザーの作成
+for ((i=0; i<=NUM_TEAMS-1; i++)); do
+  TEAM_USERNAME="team${i}"
+  TEAM_PASSWORD=$(openssl rand -base64 12 | sed 's/[+/=]/#/g')'1!'
+  aws cognito-idp admin-create-user \
+    --user-pool-id ${USER_POOL_ID} \
+    --username ${TEAM_USERNAME} \
+    --temporary-password ${TEAM_PASSWORD} \
+    --user-attributes Name="email",Value="team${i}@example.com" \
+    --region ${REGION} >/dev/null 2>&1
+
+  echo "Team ${i}: Username=${TEAM_USERNAME}, Password=${TEAM_PASSWORD}"
+done
