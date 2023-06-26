1fd0e0f90a086d9845052409510a64c140f03827
kesompochy <kesompochy@gmail.com>
2023-06-26 18:16:09 +0900

    tfstateをs3で管理したい

--- terraform/provider.tf
+++ terraform/provider.tf
@@ -1,3 +1,11 @@
 provider "aws" {
   region = "ap-northeast-1"
 }
+
+terraform {
+  backend "s3" {
+    bucket = "isu-aws-tfstate"
+    key    = "portal.tfstate"
+    region = "ap-northeast-1"
+  }
+}
