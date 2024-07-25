7b295f6b567e729136744e6fdb39a68429a46138
kesompochy <kesompochy@gmail.com>
2024-07-25 18:30:35 +0900

    sso profileを使うようにする

--- terraform/provider.tf
+++ terraform/provider.tf
@@ -1,11 +1,13 @@
 provider "aws" {
-  region = "ap-northeast-1"
+  region  = "ap-northeast-1"
+  profile = "ctoa2024"
 }
 
 terraform {
   backend "s3" {
-    bucket = "isu-aws-tfstate"
-    key    = "portal.tfstate"
-    region = "ap-northeast-1"
+    bucket  = "isu-aws-tfstate"
+    key     = "portal.tfstate"
+    region  = "ap-northeast-1"
+    profile = "ctoa2024"
   }
 }
