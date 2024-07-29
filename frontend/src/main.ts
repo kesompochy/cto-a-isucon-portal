d33ed9576f6403e9e3bf43095323c94d1644c209
kesompochy <kesompochy@gmail.com>
2024-07-29 22:42:45 +0900

    fix typo

--- frontend/src/main.ts
+++ frontend/src/main.ts
@@ -4,8 +4,8 @@ import App from './App.vue'
 import { Amplify } from 'aws-amplify'
 
 const AUTH_REGION = import.meta.env.VITE_AUTH_REGION || 'ap-northeast-1'
-const AUTH_USER_POOL_ID = import.meta.env.VITE_USER_POOL_ID || 'ap-northeast-1_owwW23cX9'
-const AUTH_USER_POOL_WEB_CLIENT_ID = import.meta.env.VITE_USER_POOL_WEB_CLIENT_ID || '28pbs1v5v549utukg74m89o2q3'
+const AUTH_USER_POOL_ID = import.meta.env.VITE_AUTH_USER_POOL_ID || 'ap-northeast-1_owwW23cX9'
+const AUTH_USER_POOL_WEB_CLIENT_ID = import.meta.env.VITE_AUTH_USER_POOL_WEB_CLIENT_ID || '28pbs1v5v549utukg74m89o2q3'
 const APPSYNC_GRAPHQL_ENDPOINT = import.meta.env.VITE_APPSYNC_GRAPHQL_ENDPOINT || 'https://6bqrafkynnbbzgkticdkxuawki.appsync-api.ap-northeast-1.amazonaws.com/graphql'
 const APPSYNC_REGION = import.meta.env.VITE_APPSYNC_REGION || 'ap-northeast-1'
 
